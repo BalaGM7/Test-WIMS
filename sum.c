c7a9000e078cf48a0ab1048a72a93f9eebdd50db
Balamanikandan <106986155+BalaGM7@users.noreply.github.com>
2022-06-07 10:10:48 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
   int a,b,total;
   printf("Enter two numbers: \n");
   scanf("%d %d", &a, &b);
   total = a+b;
   printf("Sum of %d and %d is %d", a, b, total);
+  return 0;
 }
